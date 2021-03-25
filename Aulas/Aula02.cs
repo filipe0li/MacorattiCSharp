e6153e985d2e4544ea8a11f7a6808936fc759913
Filipe <filipe_oli@hotmail.com>
2021-03-25 19:57:02 -0300

    Adicionar arquivos de projeto.

---
+++ Aulas/Aula02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacorattiCSharp.Aulas
+{
+    class Aula02
+    {
+        internal static void Aula()
+        {
+            int x;
+            int y;
+
+            x = 100;
+            Console.WriteLine("x contém o valor = " + x);
+
+            y = x / 2;
+            x = y + 100;
+
+            Console.WriteLine("y contém o valor de x / 2 = " + y);
+
+            Console.WriteLine("x contém o valor de y + 100 = " + x);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MacorattiCSharp.Aulas;
+
+namespace MacorattiCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Qual nº da aula que deseja executar?");
+                int aula;
+                int.TryParse(Console.ReadLine(), out aula);
+                Console.Clear();
+                switch (aula)
+                {
+                    case 2:
+                        Aula02.Aula();
+                        break;
+
+                    case 3:
+                    //Aula03.Aula();
+                    default:
+                        continuar = false;
+                        break;
+                }
+            }
+        }
+    }
+}
