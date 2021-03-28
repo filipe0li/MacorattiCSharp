8e34068514acf170adc3962f6236bb02941e0e55
Filipe <filipe_oli@hotmail.com>
2021-03-28 09:17:38 -0300

    Aula02
    Utilização do VAR.

---
+++ Aulas/Aula04.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MacorattiCSharp.Aulas
+{
+    class Aula04
+    {
+        internal static void Exercicio01()
+        {
+            var i = 5;
+            Console.WriteLine(i.GetType());
+
+            var nome = "Filipe";
+            Console.WriteLine(nome.GetType());
+
+            /*
+             * Não é Possivel converter um tipo int em string, pois var não siguinifica valor variavel!
+             * i = "Filipe";
+             */
+
+            var vetor = new[] { 0, 1, 2 };
+            Console.WriteLine(vetor.GetType());
+
+            var aluno = new { Nome = "Filipe" };
+            Console.WriteLine(aluno.GetType());
+
+            var lista = new List<int>();
+            Console.WriteLine(lista.GetType());
+        }
+        internal static void Exercicio02()
+        {
+            string[] nomes = { "Filipe", "Thamara" };
+            IEnumerable<string> consulta = from nome in nomes
+                                           where nome[0] == 'F'
+                                           select nome;
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+
+            var consulta1 = from nome in nomes
+                            where nome[0] == 'T'
+                            select nome;
+            foreach (var item in consulta1)
+            {
+                Console.WriteLine(item);
+            }
+
+            /*
+             * Vale a pena usar VAR
+             * ClasseComNomeMuitoGrandeDeDigitar<string> obj = new ClasseComNomeMuitoGrandeDeDigitar<string>();
+             * var obj1 = new ClasseComNomeMuitoGrandeDeDigitar<string>();
+             */
+
+            IEnumerable<Filipe> consulta2 = from nome in nomes
+                                            where nome.Length > 5
+                                            select new Filipe { Tamanho = nome.Length, Valor = nome };
+            foreach (var item in consulta2)
+            {
+                Console.WriteLine(item.Valor);
+            }
+
+            var consulta3 = from nome in nomes
+                            where nome.Length > 5
+                            select new { Tamanho = nome.Length, Valor = nome };
+
+            foreach (var item in consulta3)
+            {
+                Console.WriteLine(item.Valor);
+            }
+
+            /*
+             * Permitido normalmente:
+             * int a = (a = 20);
+             * int c = 5, d = 6;
+             */
+
+            /*
+             * var não permite:
+             * var b = (b = 20);
+             * var nome1;
+             * var nome2 = null;
+             * var e = 5, f = 6;
+             */
+        }
+    }
+    class Filipe
+    {
+        // Foi necessário devido oa não uso de var, logo foi necessário a implementção de um tipo(class).
+        public int Tamanho;
+        public string Valor;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace MacorattiCSharp
         static void Main()
         {
             var central = new Dictionary<string, Action>();
+            
+            // Aula02
             central.Add("Variaveis e Tipos - Variáveis - Ex01", Aula02.Exercicio01);
             central.Add("Variaveis e Tipos - Área do circulo - Ex02", Aula02.Exercicio02);
             central.Add("Variaveis e Tipos - Nome completo - Ex03", Aula02.Exercicio03);
@@ -17,8 +19,16 @@ namespace MacorattiCSharp
             central.Add("Nullable Types - Nullable - Ex01", Aula03.Exercicio01);
             central.Add("Nullable Types - Nullable - Ex02", Aula03.Exercicio02);
             
+            // Aula04
+            central.Add("Variaveis Implicitas Tipadas - VAR - Ex01", Aula04.Exercicio01);
+            central.Add("Variaveis Implicitas Tipadas - VAR - Ex02", Aula04.Exercicio02);
+
+            // Aula05
+
+
             CentralExercicios.SelecionarEExecutar(central);
-            Console.Write("Pressione qualquer tecla para continuar. . . "); Console.ReadLine();
+            Console.Write("Pressione qualquer tecla para continuar. . . ");
+            Console.ReadLine();
         }
     }
 }
